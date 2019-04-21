d9a836a2f09b8e92c86f0d5d5348f493f6f9b1e3
Biswash Baral <biswbara@e1z2r3p13.42.us.org>
2019-04-21 02:50:13 -0700

    is power of 2 done

---
+++ is_power_of_2/is_power_of_2.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_power_of_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: biswbara <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/21 02:36:55 by biswbara          #+#    #+#             */
+/*   Updated: 2019/04/21 02:49:49 by biswbara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int is_power_of_2(unsigned int n)
+{
+	if (n == 0)
+		return 0;
+	if (n == 1)
+		return 1;
+	while (n % 2 == 0)
+	{
+		n = n / 2;i
+		if (n == 1)
+			return 1;
+
+	}
+	return 0;
+}
+
+
+int main()
+{
+	printf("%d", is_power_of_2(4));
+	printf("%d", is_power_of_2(0));
+	printf("%d", is_power_of_2(1));
+	printf("%d", is_power_of_2(7));
+	printf("%d", is_power_of_2(16));
+	printf("%d", is_power_of_2(32));
+}
